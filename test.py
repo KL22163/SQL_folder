64775b350385ee370dc4d08a7da5cad7fc28553f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-11 12:46:01 +1200

    I've added a docstring and some other stuff.

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
+"""A code which tells to the top speeds of the fastest cars:
+A code by Katrina Lai 11/04/24"""
 import sqlite3
 
-db = sqlite3.connect('cars.db')
-cursor = db.cursor()
-sql = 'SELECT * FROM Car;'
-cursor.execute(sql)
+DATABASE = 'cars.db'
 
-db.close()
+def print_all_cars():
+    speed = input("")
+    with sqlite3.connect(DATABASE) as db:
+        cursor = db.cursor()
+        sql = 'SELECT car_name FROM car WHERE top_speed > 250;'
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        #print them nicely
+        for car in results:
+            print(f"Car: {car[1]}, Top Speed: {car[2]}")
+
+if __name__ == "__main__":
+    print_all_cars()
