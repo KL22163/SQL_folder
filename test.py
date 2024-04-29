ba61724b55a8cb178002d23b89e9c15d289943d9
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-29 13:59:43 +1200

    I have now finished my code.

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ import sqlite3
 DATABASE = 'cars.db'
 
 def print_all_cars():
-    speed = input("")
+    speed = input("What speed: ")
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
-        sql = 'SELECT car_name FROM car WHERE top_speed > 250;'
-        cursor.execute(sql)
+        sql = "SELECT car_name, top_speed FROM car WHERE top_speed > ?;"
+        cursor.execute(sql,(speed,))
         results = cursor.fetchall()
         #print them nicely
         for car in results:
-            print(f"Car: {car[1]}, Top Speed: {car[2]}")
+            print(f"Car: {car[0]}, Top Speed: {car[1]}")
 
 if __name__ == "__main__":
     print_all_cars()
