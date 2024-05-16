7457ef3a23e1d638c430c87d135b1565fce335ee
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-16 14:16:28 +1200

    I have started my python code (search by name)

--- poke_type.py
+++ poke_type.py
@@ -1,1 +1,7 @@
-print('Hello, world!')
+import sqlite3
+
+DATABASE = "poke_type.db"
+
+def print_all_pokemon():
+    type = input("Search by what type/s?: ")
+    with sqlite3.connect(DATABASE) as db
