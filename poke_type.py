9ae7f8f5f82f5586b9f11755b1601ce85cc39408
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-20 13:25:56 +1200

    Trying to complete the search by name function

--- poke_type.py
+++ poke_type.py
@@ -1,7 +1,18 @@
+#Selecting the SQL database
 import sqlite3
-
 DATABASE = "poke_type.db"
 
+#Asking what pokemon types to sort by, search by type
 def print_all_pokemon():
-    type = input("Search by what type/s?: ")
-    with sqlite3.connect(DATABASE) as db
+    type_1 = input("Type 1: ")
+    #type_2 = input("Type 2: ")
+    with sqlite3.connect(DATABASE) as db:
+        cursor = db.cursor()
+        sql = "SELECT poke_name FROM Typings WHERE type_name = ?;"
+        cursor.execute(sql,(type_1))
+        results = cursor.fetchall()
+        for pokemons in results:
+            print(pokemons)
+
+
+print_all_pokemon()
