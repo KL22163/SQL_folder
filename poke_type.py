df953f9f4e2bbafcc6fcd7176b3e5a24a4ddc68d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-21 13:25:59 +1200

    Trying to join type id and type names now.

--- poke_type.py
+++ poke_type.py
@@ -4,11 +4,11 @@ DATABASE = "poke_type.db"
 
 #Asking what pokemon types to sort by, search by type
 def print_all_pokemon():
-    type_1 = input("Type 1: ")
+    type_1 = int(input("Type 1: "))
     #type_2 = input("Type 2: ")
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
-        sql = "SELECT poke_name FROM Typings WHERE type_name = ?;"
+        sql = "SELECT poke_type.db FROM Pokemons INNER JOIN Typings ON Typings.id = Typings.type_name WHERE type_1 = ?;"
         cursor.execute(sql,(type_1))
         results = cursor.fetchall()
         for pokemons in results:
