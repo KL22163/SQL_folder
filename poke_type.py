096c029594a6fd8bf2f34ad8508be3da24b7c207
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-22 14:15:03 +1200

    I did it!!! I need to deal with the nulls though..

--- poke_type.py
+++ poke_type.py
@@ -1,16 +1,15 @@
 #Selecting the SQL database
 import sqlite3
-DATABASE = "poke_type (1).db"
-
+DATABASE = "poke_type.db"
 
 #Asking what pokemon types to sort by, search by type
 def print_all_pokemon():
     type_1 = int(input("Type 1: "))
-    #type_2 = input("Type 2: ")
+    type_2 = int(input("Type 2: "))
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
-        sql = "SELECT Pokemons.poke_name FROM Pokemons WHERE Pokemons.type_1 = ?;"
-        cursor.execute(sql,(type_1,))
+        sql = "SELECT Pokemons.poke_name FROM Pokemons WHERE Pokemons.type_1 = ? AND Pokemons.type_2 = ? OR Pokemons.type_1 = ? AND Pokemons.type_2 = ?;"
+        cursor.execute(sql,(type_1, type_2, type_2, type_1))
         results = cursor.fetchall()
         for pokemons in results:
             print(pokemons[0])
