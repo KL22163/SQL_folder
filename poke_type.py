7fdd52d16434b70e67ba7f1249a6f9806b9fcf85
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-27 14:16:50 +1200

    Fixed code, works with one type now.

--- poke_type.py
+++ poke_type.py
@@ -4,16 +4,24 @@ DATABASE = "poke_type.db"
 
 #Asking what pokemon types to sort by, search by type
 def print_all_pokemon():
-    type_1 = int(input("Type 1: "))
-    type_2 = int(input("Type 2: "))
-    with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
-        sql = "SELECT Pokemons.poke_name FROM Pokemons WHERE Pokemons.type_1 = ? AND Pokemons.type_2 = ? OR Pokemons.type_1 = ? AND Pokemons.type_2 = ?;"
-        cursor.execute(sql,(type_1, type_2, type_2, type_1))
-        results = cursor.fetchall()
-        for pokemons in results:
-            print(pokemons[0])
+    type_1 = input("Type 1: ")
+    type_2 = input("Type 2: ")
+    if type_2 == "":
+        with sqlite3.connect(DATABASE) as db:
+            cursor = db.cursor()
+            sql = "SELECT Pokemons.poke_name FROM Pokemons WHERE Pokemons.type_1 = ? OR Pokemons.type_2 = ?;"
+            cursor.execute(sql,(type_1, type_1))
+            results = cursor.fetchall()
+            for pokemons in results:
+                print(pokemons[0])
+    else:
+        with sqlite3.connect(DATABASE) as db:
+            cursor = db.cursor()
+            sql = "SELECT Pokemons.poke_name FROM Pokemons WHERE Pokemons.type_1 = ? AND Pokemons.type_2 = ? OR Pokemons.type_1 = ? AND Pokemons.type_2 = ?;"
+            cursor.execute(sql,(type_1, type_2, type_2, type_1))
+            results = cursor.fetchall()
+            for pokemons in results:
+                print(pokemons[0])
 
 
-if __name__ == "__main__":
-    print_all_pokemon()
+print_all_pokemon()
