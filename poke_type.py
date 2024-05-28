131a3136cbd445347dc76d4dc650bfdd8dc9380d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-28 13:28:17 +1200

    My program can now show all the pokemon types.

--- poke_type.py
+++ poke_type.py
@@ -2,10 +2,21 @@
 import sqlite3
 DATABASE = "poke_type.db"
 
+user_input = input("What would you like to do? /n")
+#Showing the type and corrseponding number
+with sqlite3.connect(DATABASE) as db:
+    cursor = db.cursor()
+    print("Please input the number corresponding to the pokemon type:")
+    sql = "SELECT * FROM Typings"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for types in results:
+        print(types[0], types[1])
 #Asking what pokemon types to sort by, search by type
-def print_all_pokemon():
+def pokemon_of_type():
     type_1 = input("Type 1: ")
     type_2 = input("Type 2: ")
+#If input is only one type:
     if type_2 == "":
         with sqlite3.connect(DATABASE) as db:
             cursor = db.cursor()
@@ -14,14 +25,16 @@ def print_all_pokemon():
             results = cursor.fetchall()
             for pokemons in results:
                 print(pokemons[0])
+    #If input has two types:
     else:
         with sqlite3.connect(DATABASE) as db:
             cursor = db.cursor()
             sql = "SELECT Pokemons.poke_name FROM Pokemons WHERE Pokemons.type_1 = ? AND Pokemons.type_2 = ? OR Pokemons.type_1 = ? AND Pokemons.type_2 = ?;"
             cursor.execute(sql,(type_1, type_2, type_2, type_1))
+            #Print results
             results = cursor.fetchall()
             for pokemons in results:
                 print(pokemons[0])
+pokemon_of_type()
 
-
-print_all_pokemon()
+#Asking which pokemon to search by, search by name
